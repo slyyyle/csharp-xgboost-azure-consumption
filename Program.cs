eabe8cc11373b2be829245cfdbc2c5fb72beb822
slyyyle <khammitt1@gmail.com>
2023-06-01 21:56:31 -0500

    published repo

---
+++ Program.cs
@@ -0,0 +1,92 @@
+// This code requires the Nuget package Microsoft.AspNet.WebApi.Client to be installed.
+// Instructions for doing this in Visual Studio:
+// Tools -> Nuget Package Manager -> Package Manager Console
+// Install-Package Newtonsoft.Json
+// .NET Framework 4.7.1 or greater must be used
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CallRequestResponseService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InvokeRequestResponseService().Wait();
+        }
+
+        static async Task InvokeRequestResponseService()
+        {
+            var handler = new HttpClientHandler()
+            {
+                ClientCertificateOptions = ClientCertificateOption.Manual,
+                ServerCertificateCustomValidationCallback =
+                        (httpRequestMessage, cert, cetChain, policyErrors) => { return true; }
+            };
+            using (var client = new HttpClient(handler))
+            {
+                // Request data goes here
+                // The example below assumes JSON formatting which may be updated
+                // depending on the format your endpoint expects.
+                // More information can be found here:
+                // https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
+                var requestBody = @"{
+                  ""input_data"": {""columns"":[0,1,2,3,4,5,6,7],
+                                     ""index"":[0],
+                                    ""data"":[[0,88,0,0,0,55,6.3,280]]}
+                }";
+
+                // Replace this with the primary/secondary key or AMLToken for the endpoint
+                const string apiKey = "REMOVED FOR PRIVACY";
+                if (string.IsNullOrEmpty(apiKey))
+                {
+                    throw new Exception("A key should be provided to invoke the endpoint");
+                }
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+                client.BaseAddress = new Uri("https://xgboost-endpoint-api.eastus2.inference.ml.azure.com/score");
+
+                var content = new StringContent(requestBody);
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                // This header will force the request to go to a specific deployment.
+                // Remove this line to have the request observe the endpoint traffic rules
+                content.Headers.Add("azureml-model-deployment", "diabetes-xgboost-model-5");
+
+                // WARNING: The 'await' statement below can result in a deadlock
+                // if you are calling this code from the UI thread of an ASP.Net application.
+                // One way to address this would be to call ConfigureAwait(false)
+                // so that the execution does not attempt to resume on the original context.
+                // For instance, replace code such as:
+                //      result = await DoSomeTask()
+                // with the following:
+                //      result = await DoSomeTask().ConfigureAwait(false)
+                HttpResponseMessage response = await client.PostAsync("", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Result: {0}", result);
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
+
+                    // Print the headers - they include the requert ID and the timestamp,
+                    // which are useful for debugging the failure
+                    Console.WriteLine(response.Headers.ToString());
+
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseContent);
+                }
+            }
+        }
+    }
+}
+
